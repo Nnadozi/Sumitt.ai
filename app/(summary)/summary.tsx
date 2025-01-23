bd81fc33c614a3a593a12cf530fa4c871cf029b2
Zixon77 <chikaosro@gmail.com>
2025-01-23 12:56:47 -0500

    iplease

--- app/(summary)/summary.tsx
+++ app/(summary)/summary.tsx
@@ -13,7 +13,7 @@ import NameModule from '@/components/NameModule';
 import * as NetInfo from '@react-native-community/netinfo';
 import { InterstitialAd, AdEventType, TestIds } from 'react-native-google-mobile-ads';
 
-const interstitialAd = InterstitialAd.createForAdRequest("ca-app-pub-8705039555355167/4122975239", {
+const interstitialAd = InterstitialAd.createForAdRequest("ca-app-pub-8501095031703685/3736822220", {
   requestNonPersonalizedAdsOnly: true,
 });
 
